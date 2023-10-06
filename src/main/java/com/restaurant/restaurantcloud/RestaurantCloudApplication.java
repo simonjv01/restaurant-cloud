b8c6344ed95b2eb5e69c30a9ca797f2496c4fae3
Simon Vargas <sjvargas01@gmail.com>
2023-10-06 14:09:25 -0400

    first commit

---
+++ src/main/java/com/restaurant/restaurantcloud/RestaurantCloudApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.restaurantcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCloudApplication.class, args);
+	}
+
+}
